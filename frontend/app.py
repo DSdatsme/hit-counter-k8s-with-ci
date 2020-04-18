8666212364c2a8f1b859bebf3b03a92a964999f5
DSdatsme <darsuratwala@gmail.com>
2020-04-18 19:03:21 +0530

    first commit

---
+++ frontend/app.py
@@ -0,0 +1,35 @@
+import time
+import redis
+from flask import Flask
+import logging
+import watchtower
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis-master', port=6379)
+
+# setup cloudwatch logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(watchtower.CloudWatchLogHandler()) # fetch from env AWS configuration
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hit():
+    count = get_hit_count()
+    logger.info("website visited!")
+    return 'I have been hit %i times since deployment.\n' % int(count)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)y
